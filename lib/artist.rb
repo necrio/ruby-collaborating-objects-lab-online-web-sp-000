656909ff89559086152be0e43deed3ccff36d5f8
Keith Isham <kisham8899@gmail.com>
2019-05-16 16:38:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -117,10 +117,10 @@ class MP3Importer
     attr_accessor :path
 
     def initialize(path)
-      @path = path 
+      @path = path
     end
 
-  
+
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
