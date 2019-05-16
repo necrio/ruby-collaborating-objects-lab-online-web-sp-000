7c136ac71566dd9897c6d93a42eddbe31b960e8f
Keith Isham <kisham8899@gmail.com>
2019-05-16 16:09:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  # other methods
+ 
+end
+
+
+
+class Song
+  attr_accessor :title
+ 
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+ 
+end
+ 
+class MP3Importer
+  def import(list_of_filenames)
+    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  end
+end
