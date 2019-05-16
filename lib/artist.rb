e24b257121d1be0750bfe8b115dee2159cc41ab5
Keith Isham <kisham8899@gmail.com>
2019-05-16 16:40:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -120,8 +120,19 @@ class MP3Importer
       @path = path
     end
 
+    def files
+      files = []
+      Dir.new(self.path).each do |file|
+        files << file if file.length > 4
+      end
+      files
+    end
 
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
-  end
+
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+
 end
