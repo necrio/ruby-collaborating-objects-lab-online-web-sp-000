b0ee8e6016981ab077c57cc09d3c5b36def16866
Keith Isham <kisham8899@gmail.com>
2019-05-16 16:44:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,7 @@ class Artist
 
 
   def add_song(song)
-self.songs << song
+@songs << song
   end
 
   def save
@@ -45,7 +45,7 @@ self.songs << song
   def print_the_songs
     self.songs.each {|song| song.name}
   end
-
+  
 
 
 
